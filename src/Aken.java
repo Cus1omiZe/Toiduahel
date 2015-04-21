41963a95d138ca7fb570f678bf4c499921261461
MasTaVi <bomba389@mail.ru>
2015-04-21 11:48:16 +0300

    Changes to code

--- src/Aken.java
+++ src/Aken.java
@@ -5,37 +5,62 @@ import javafx.scene.layout.*;
 import javafx.stage.*;
 import java.io.*;
 import java.util.*;
+import javafx.animation.*;
+import javafx.util.Duration;
 
 public class Aken extends Application
 {
-    int hunte=5;
-    int janeseid=30;
-    TextField tfHunte=new TextField(String.valueOf(hunte));
-    TextField tfJaneseid=new TextField(String.valueOf(janeseid));
-    Button nupp1=new Button("1 p?ev");
-    Button salvestusnupp=new Button("Salvesta");
-    Button lugemisnupp=new Button("Loe");
+    int hunte = 5;
+    int janeseid = 30;
+    TextField tfHunte = new TextField(String.valueOf(hunte));
+    TextField tfJaneseid = new TextField(String.valueOf(janeseid));
+    Button nupp1 = new Button("1 p?ev");
+    Button salvestusnupp = new Button("Salvesta");
+    Button lugemisnupp = new Button("Loe");
+    Timeline ajatiksuja = new Timeline(new KeyFrame(Duration.seconds(2), (event)->arvutaPaev()));
     public void start(Stage stage)
     {
-        VBox vb=new VBox();
-        HBox hb=new HBox();
+        VBox vb = new VBox();
+        HBox hb = new HBox();
         hb.getChildren().addAll(tfHunte, tfJaneseid);
-        HBox nupud=new HBox();
+        HBox nupud = new HBox();
         nupud.getChildren().addAll(nupp1, salvestusnupp, lugemisnupp);
         vb.getChildren().addAll(hb, nupud);
-        Group juur=new Group(vb);
+        Group juur = new Group(vb);
         stage.setScene(new Scene(juur));
         stage.show();
         salvestusnupp.setOnAction((event) -> salvesta());
+        lugemisnupp.setOnAction((event) -> loe());
+        nupp1.setOnAction((event) -> arvutaPaev());
+        ajatiksuja.setCycleCount(Timeline.INDEFINITE);
+        ajatiksuja.play();
+    }
+
+    void arvutaPaev()
+    {
+        tekstistMuutujatesse();
+        if(janeseid>= hunte){janeseid-= hunte;}
+        muutujatestTekstivaljadesse();
+    }
+    void tekstistMuutujatesse()
+    {
+        hunte = Integer.parseInt(tfHunte.getText());
+        janeseid = Integer.parseInt(tfJaneseid.getText());
+    }
+    void muutujatestTekstivaljadesse()
+    {
+        tfHunte.setText(String.valueOf(hunte));
+        tfJaneseid.setText(String.valueOf(janeseid));
     }
     void salvesta()
     {
         try
         {
-            Properties prop=new Properties();
+            tekstistMuutujatesse();
+            Properties prop = new Properties();
             prop.put("hunte", String.valueOf(hunte));
             prop.put("janeseid", String.valueOf(janeseid));
-            FileOutputStream fos=new FileOutputStream("andmed.txt");
+            FileOutputStream fos = new FileOutputStream("andmed.txt");
             prop.store(fos, "Metsaandmed");
             fos.close();
         }
@@ -43,5 +68,17 @@ public class Aken extends Application
         {
             ex.printStackTrace();
         }
+    }
+    void loe()
+    {
+        try
+        {
+            Properties prop = new Properties();
+            prop.load(new FileInputStream("andmed.txt"));
+            hunte = Integer.parseInt(prop.getProperty("hunte"));
+            janeseid = Integer.parseInt(prop.getProperty("janeseid"));
+            muutujatestTekstivaljadesse();
+        }
+        catch(IOException ex){ex.printStackTrace();}
     }
 }
