c713601e894e0a04715be73a5ff0b3f363d7add3
MasTaVi <bomba389@mail.ru>
2015-04-21 11:15:06 +0300

    Changes to code

--- src/Aken.java
+++ src/Aken.java
@@ -1,44 +1,45 @@
 import javafx.application.*;
 import javafx.scene.*;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.*;
-import javafx.scene.control.*;
 import java.io.*;
 import java.util.*;
 
 public class Aken extends Application
 {
-    int hunte = 5;
-    int janesid = 30;
-    TextField tfHunte = new TextField(String.valueOf(hunte));
-    TextField tfJaneseid = new TextField(String.valueOf(janesid));
-    Button nupp1 = new Button("1 p?ev");
-    Button salvestusnupp = new Button("Salvesta");
-    Button lugemisnupp = new Button("Loe");
+    int hunte=5;
+    int janeseid=30;
+    TextField tfHunte=new TextField(String.valueOf(hunte));
+    TextField tfJaneseid=new TextField(String.valueOf(janeseid));
+    Button nupp1=new Button("1 p?ev");
+    Button salvestusnupp=new Button("Salvesta");
+    Button lugemisnupp=new Button("Loe");
     public void start(Stage stage)
     {
-        VBox vb = new VBox();
-        HBox hb = new HBox();
+        VBox vb=new VBox();
+        HBox hb=new HBox();
         hb.getChildren().addAll(tfHunte, tfJaneseid);
-        HBox nupud = new HBox();
+        HBox nupud=new HBox();
         nupud.getChildren().addAll(nupp1, salvestusnupp, lugemisnupp);
         vb.getChildren().addAll(hb, nupud);
-        Group juur = new Group(vb);
+        Group juur=new Group(vb);
         stage.setScene(new Scene(juur));
         stage.show();
+        salvestusnupp.setOnAction((event) -> salvesta());
     }
     void salvesta()
     {
         try
         {
-            Properties prop = new Properties();
-            prop.put("hunte", hunte);
-            prop.put("janesid", janesid);
-            FileOutputStream fos = new FileOutputStream("andmed.txt");
+            Properties prop=new Properties();
+            prop.put("hunte", String.valueOf(hunte));
+            prop.put("janeseid", String.valueOf(janeseid));
+            FileOutputStream fos=new FileOutputStream("andmed.txt");
             prop.store(fos, "Metsaandmed");
             fos.close();
         }
-        catch (IOException ex)
+        catch(IOException ex)
         {
             ex.printStackTrace();
         }
